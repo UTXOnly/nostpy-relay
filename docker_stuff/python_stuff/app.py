f2efaf22e847bf1218ee0e99e56348e5f08c2f0c
Brian Hartford <bhartford419@gmail.com>
2023-05-07 17:32:35 -0400

    remove close from EOSE

--- docker_stuff/python_stuff/app.py
+++ docker_stuff/python_stuff/app.py
@@ -115,10 +115,10 @@ async def handle_websocket_connection(websocket, path):
             subscription_id = message_list[1]
             response = "NOTICE", f"closing {subscription_id}"
             if origin == "https://iris.to":
-                logger.debug(f"Sending EOSE Response: {json.dumps(response)}")
+                logger.debug(f"Sending CLOSE Response: {json.dumps(response)}")
                 await websocket.send(json.dumps(response))
             else:
-                logger.debug(f"Sending EOSE Response: {json.dumps(response)} and closing websocket")
+                logger.debug(f"Sending CLOSE Response: {json.dumps(response)} and closing websocket")
                 await websocket.send(json.dumps(response))
                 await websocket.close()
         else:
@@ -163,8 +163,9 @@ async def handle_subscription_request(subscription_dict, websocket, subscription
         EOSE = "EOSE", subscription_id
         logger.debug(f"EOSE Resonse = {json.dumps(EOSE)}")
         await websocket.send(json.dumps(EOSE))
-        #if origin != "https://iris.to":
-        #    await websocket.close()
+        if origin != "https://iris.to":
+            logger.debug("Closing non Iris websocket")
+            await websocket.close()
 
 
 if __name__ == "__main__":
