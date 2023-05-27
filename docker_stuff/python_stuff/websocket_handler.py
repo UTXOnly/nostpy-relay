4af2488bd852596978be283c4c8c561636a69c4e
Brian Hartford <bhartford419@gmail.com>
2023-05-27 14:46:55 -0400

    remove results.ites() to iterates over just hte list

--- docker_stuff/python_stuff/websocket_handler.py
+++ docker_stuff/python_stuff/websocket_handler.py
@@ -71,7 +71,7 @@ async def send_subscription_to_handler(session, event_dict, subscription_id, ori
 
             if response.status == 200:
                 logger.debug(f"Sending response data: {response_data}")
-                for event_item in results.items():
+                for event_item in results:
                     client_response = "EVENT" , subscription_id, event_item
                     await websocket.send(json.dumps(client_response))
 
