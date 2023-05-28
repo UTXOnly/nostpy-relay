5c9e04e85557832638c3ccd046a74785d9877516
Brian Hartford <bhartford419@gmail.com>
2023-05-28 01:35:33 -0400

    back to json dumps but do not return EOSE to snort

--- docker_stuff/python_stuff/websocket_handler.py
+++ docker_stuff/python_stuff/websocket_handler.py
@@ -78,19 +78,16 @@ async def send_subscription_to_handler(session, event_dict, subscription_id, ori
                 if event_type == "EOSE":
                     client_response = event_type, subscription_id
                     if origin == "https://snort.social":
-                        await websocket.send(client_response)
+                        pass
                     else:
                         await websocket.send(json.dumps(client_response))
                 else:
                     for event_item in results:
                         client_response = event_type , subscription_id, event_item
-                        if origin == "https://snort.social":
-                            await websocket.send(client_response)
-                        else:
-                            await websocket.send(json.dumps(client_response))
+                        await websocket.send(json.dumps(client_response))
 
                 if origin == "https://snort.social":
-                    await websocket.send(EOSE)
+                    pass
                 else:
                     await websocket.send(json.dumps(EOSE))  
             else:
