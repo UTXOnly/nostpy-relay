86376be100cdc54e5a4d90654f684355a1384c88
UTXOnly <brian.hartford@datadoghq.com>
2023-07-02 16:57:30 -0400

    Cleanup

--- docker_stuff/python_stuff/event_handler.py
+++ docker_stuff/python_stuff/event_handler.py
@@ -163,10 +163,6 @@ async def event_query(filters):
                 query_result = query.order_by(desc(Event.created_at)).limit(query_limit).all()
                 serialized_events = []
                 serialized_events = [serialize(event) for event in query_result]
-                #for event in query_result:
-                #    serialized_event = serialize(event)
-                #    serialized_events.append(serialized_event)
-    
                 redis_set = redis_client.set(redis_get, str(serialized_events), ex=3600)  # Set cache expiry time to 1 hour
                 logger.debug(f"Query result stored in cache. Stored as: {redis_set} ({inspect.currentframe().f_lineno})")
             except Exception as e:
