33fa454e5c2bc6d70eb4132bd52c107c31a2d888
Brian Hartford <bhartford419@gmail.com>
2024-03-09 11:44:33 -0500

    If event type extract message

--- docker_stuff/python_stuff/websocket_handler.py
+++ docker_stuff/python_stuff/websocket_handler.py
@@ -168,8 +168,9 @@ class ExtractedResponse:
                 f"Error decoding JSON message in Extracted Response: {json_error}."
             )
             self.results = ""
-
-        self.message = response_data.get("message", "")
+            
+        if self.event_type == "EVENT":
+            self.message = response_data.get("message", "")
 
 
         self.comment = ""
