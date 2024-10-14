bf42917f53c14936dc9ad4a742cb4858a6a8aa6f
Brian Hartford <bhartford419@gmail.com>
2024-10-13 20:39:26 -0400

    manual wot run

--- docker/nostpy_relay/wot_builder.py
+++ docker/nostpy_relay/wot_builder.py
@@ -15,7 +15,7 @@ from asyncpg import create_pool
 
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s %(levelname)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
