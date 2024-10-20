80d2003a529150f1069f076eadd07536b76c20ee
Brian Hartford <bhartford419@gmail.com>
2024-10-20 10:37:23 -0400

    Add custom OTel metrics (#77)

--- docker/nostpy_relay/event_handler.py
+++ docker/nostpy_relay/event_handler.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 from contextlib import asynccontextmanager
+from typing import Iterable, Callable, Dict, List
 
 import psycopg
 import redis
@@ -10,22 +11,21 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from opentelemetry import trace
-from opentelemetry._logs import set_logger_provider
-from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.psycopg import PsycopgInstrumentor
 from opentelemetry.instrumentation.redis import RedisInstrumentor
-from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.metrics import Observation
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.semconv.trace import SpanAttributes
+from otel_metric_base.otel_metrics import OtelMetricBase
 from psycopg_pool import AsyncConnectionPool
 
 from event_classes import Event, Subscription
 from init_db import initialize_db
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -39,26 +39,6 @@ WOT_ENABLED = os.getenv("WOT_ENABLED")
 
 app = FastAPI()
 
-# Set up logging
-logger_provider = LoggerProvider(
-    resource=Resource.create({"service.name": "event_handler_otel"})
-)
-set_logger_provider(logger_provider)
-
-log_exporter = OTLPLogExporter(
-    endpoint=os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT"), insecure=True
-)
-logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
-
-handler = LoggingHandler(
-    level=logging.INFO,
-    logger_provider=logger_provider,
-)
-
-# Create a single logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
 
 trace.set_tracer_provider(
     TracerProvider(resource=Resource.create({"service.name": "event_handler_otel"}))
@@ -84,6 +64,41 @@ redis_tracer_provider.add_span_processor(redis_span_processor)
 # Instrument Redis with the separate tracer provider
 RedisInstrumentor().instrument(tracer_provider=redis_tracer_provider)
 redis_client = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"))
+
+otel_metrics = OtelMetricBase(otlp_endpoint=os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT"))
+metric_counters = {
+    "wot_event_reject": {},
+    "event_added": {},
+    "event_query": {},
+}
+
+
+def increment_counter(tags: Dict[str, str], counter_dict: Dict[str, Dict[str, any]]):
+    tag_key = str(sorted(tags.items()))  # Convert dict to a unique key
+    counter_dict.setdefault(tag_key, {"count": 0, "tags": tags})["count"] += 1
+
+
+def create_observable_callback(counter_dict: Dict[str, Dict[str, any]]) -> Callable:
+    def observable_callback(_):
+        return [
+            Observation(entry["count"], entry["tags"])
+            for entry in counter_dict.values()
+        ]
+
+    return observable_callback
+
+
+def register_metric(name: str, description: str):
+    otel_metrics.meter.create_observable_counter(
+        name=name,
+        description=description,
+        callbacks=[create_observable_callback(metric_counters[name])],
+    )
+
+
+register_metric("wot_event_reject", "Rejected note from WoT filter")
+register_metric("event_added", "Event added")
+register_metric("event_query", "Event query")
 
 
 def get_conn_str(db_suffix: str) -> str:
@@ -151,7 +166,7 @@ async def handle_new_event(request: Request) -> JSONResponse:
         sig=event_dict["sig"],
     )
     logger.debug(
-        f"New event loop iter, ev object is {event_obj.event_id} and {event_obj.kind}"
+        f"New event loop iter, event id is {event_obj.event_id} and kind is {event_obj.kind}"
     )
 
     try:
@@ -169,31 +184,23 @@ async def handle_new_event(request: Request) -> JSONResponse:
 
             async with request.app.write_pool.connection() as conn:
                 async with conn.cursor() as cur:
+                    otel_tags = {
+                        "kind": event_obj.kind,
+                        "pubkey": event_obj.pubkey,
+                        "event_id": event_obj.event_id,
+                    }
                     if WOT_ENABLED in ["True", "true"]:
                         wot_check = await event_obj.check_wot(cur)
-                        if wot_check:
-                            logger.debug(
-                                f"Allow check passed: {wot_check}, adding event id: {event_obj.event_id}"
-                            )
-                            await event_obj.add_event(conn, cur)
-                            return event_obj.evt_response(
-                                results_status="true", http_status_code=200
-                            )
-                        else:
+                        if not wot_check:
                             logger.debug(f"allow check failed: {wot_check}")
+                            increment_counter(
+                                otel_tags, metric_counters["wot_event_reject"]
+                            )
                             return event_obj.evt_response(
                                 results_status="false",
                                 http_status_code=403,
                                 message="rejected: user is not in relay's web of trust",
                             )
-                    if event_obj.kind == 42069:
-                        await event_obj.add_mgmt_event(conn, cur)
-                        clt_msg = await event_obj.parse_mgmt_event(conn, cur)
-                        return event_obj.evt_response(
-                            results_status="true",
-                            http_status_code=200,
-                            message=clt_msg,
-                        )
 
                     if event_obj.kind in [0, 3]:
                         await event_obj.delete_check(conn, cur)
@@ -203,7 +210,7 @@ async def handle_new_event(request: Request) -> JSONResponse:
                             results_status="true", http_status_code=200
                         )
 
-                    elif event_obj.kind == 5:
+                    if event_obj.kind == 5:
                         events_to_delete = event_obj.parse_kind5()
                         await event_obj.delete_event(conn, cur, events_to_delete)
                         return event_obj.evt_response(
@@ -213,6 +220,7 @@ async def handle_new_event(request: Request) -> JSONResponse:
                     else:
                         try:
                             await event_obj.add_event(conn, cur)
+                            increment_counter(otel_tags, metric_counters["event_added"])
                         except psycopg.IntegrityError:
                             await conn.rollback()
                             logger.info(
@@ -262,23 +270,8 @@ async def handle_subscription(request: Request) -> JSONResponse:
             global_search,
         ) = await subscription_obj.parse_filters(subscription_obj.filters, logger)
 
-        if subscription_obj.subscription_id == "nostpy_client":
-            sql_query = "SELECT client_pub, kind , allowed, note_id from allowlist;"
-            query_results = await execute_sql_with_tracing(
-                app, sql_query, "select Allow"
-            )
-            if query_results:
-                parsed_results = await subscription_obj.query_result_parser_hard(
-                    query_results
-                )
-                serialized_events = json.dumps(parsed_results)
-                redis_client.setex(str(raw_filters_copy), 240, serialized_events)
-                logger.debug(
-                    f"Caching results, keys: {str(raw_filters_copy)} value is: {serialized_events}"
-                )
-                return subscription_obj.sub_response_builder(
-                    "EVENT", subscription_obj.subscription_id, serialized_events, 200
-                )
+        query_tags = {"env": "pre-cache"}
+        increment_counter(query_tags, metric_counters["event_query"])
 
         cached_results = subscription_obj.fetch_data_from_cache(
             str(raw_filters_copy), redis_client
